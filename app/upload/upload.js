f8b71e4f180e38b12d755931b6a0aca8db792eb5
troy <troy@daisyworks.com>
2018-05-02 06:55:57 -0400

    updated name to prefix instead of postfix date for uniqueness since it was not ideal WRT extension

--- app/upload/upload.js
+++ app/upload/upload.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
     cb(null, './uploads')
   },
   filename(req, file, cb) {
-    cb(null, `${file.originalname}-${Date.now()}`)
+    cb(null, `${Date.now()}-${file.originalname}`)
   },
 })
 
